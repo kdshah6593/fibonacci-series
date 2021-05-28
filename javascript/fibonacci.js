b4e8acca4db2408ab633e2127065ad49ea3095f5
Kunal <kdshah6593@gmail.com>
2021-05-28 13:50:50 -0400

    js solution

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
   // type your code here
+  let current = 0;
+  let value1 = 0;
+  let value2 = 1;
+  let i = 0;
+  while (i <= num) {
+    current = current + value1;
+    value1 = value2
+    value2 = current
+    i += 1;
+  }
+  return current;
 }
 
 if (require.main === module) {
@@ -21,4 +32,9 @@ if (require.main === module) {
 module.exports = fibonacci;
 
 // Please add your pseudocode to this file
+
+// set first value to 0 second value to 1, then add sums together and set that number as the new second number and the second number
+// as the new first number
+// do this in a while loop until hit the nth element in the series then return that value
+
 // And a written explanation of your solution
